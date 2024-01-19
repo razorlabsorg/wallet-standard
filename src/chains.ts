50cbf799f2bb113c2b58a8c112302b07c24708ed
Nate <89227631+natedotv2@users.noreply.github.com>
2024-01-19 14:32:19 +0100

    chore: New prettier linting

--- src/chains.ts
+++ src/chains.ts
@@ -1,44 +1,44 @@
 /** M1 Devnet */
-export const M1_DEVNET_CHAIN = 'movement:m1:devnet';
+export const M1_DEVNET_CHAIN = 'movement:m1:devnet'
 
 /** M1 Testnet */
-export const M1_TESTNET_CHAIN = 'movement:m1:testnet';
+export const M1_TESTNET_CHAIN = 'movement:m1:testnet'
 
 /** M1 Localnet */
-export const M1_LOCALNET_CHAIN = 'movement:m1:localnet';
+export const M1_LOCALNET_CHAIN = 'movement:m1:localnet'
 
 /** M1 Mainnet */
-export const M1_MAINNET_CHAIN = 'movement:m1:mainnet';
+export const M1_MAINNET_CHAIN = 'movement:m1:mainnet'
 
 /** M2 Devnet */
-export const M2_DEVNET_CHAIN = 'movement:m2:devnet';
+export const M2_DEVNET_CHAIN = 'movement:m2:devnet'
 
 /** M2 Testnet */
-export const M2_TESTNET_CHAIN = 'movement:m2:testnet';
+export const M2_TESTNET_CHAIN = 'movement:m2:testnet'
 
 /** M2 Localnet */
-export const M2_LOCALNET_CHAIN = 'movement:m2:localnet';
+export const M2_LOCALNET_CHAIN = 'movement:m2:localnet'
 
 /** M2 Mainnet */
-export const M2_MAINNET_CHAIN = 'movement:m2:mainnet';
+export const M2_MAINNET_CHAIN = 'movement:m2:mainnet'
 
 export const MOVEMENT_CHAINS = [
-	M1_DEVNET_CHAIN,
-	M1_TESTNET_CHAIN,
-	M1_LOCALNET_CHAIN,
-	M1_MAINNET_CHAIN,
-	M2_DEVNET_CHAIN,
-	M2_TESTNET_CHAIN,
-	M2_LOCALNET_CHAIN,
-	M2_MAINNET_CHAIN,
-] as const;
+  M1_DEVNET_CHAIN,
+  M1_TESTNET_CHAIN,
+  M1_LOCALNET_CHAIN,
+  M1_MAINNET_CHAIN,
+  M2_DEVNET_CHAIN,
+  M2_TESTNET_CHAIN,
+  M2_LOCALNET_CHAIN,
+  M2_MAINNET_CHAIN
+] as const
 
 export type MovementChain =
-	| typeof M1_DEVNET_CHAIN
-	| typeof M1_TESTNET_CHAIN
-	| typeof M1_LOCALNET_CHAIN
-	| typeof M1_MAINNET_CHAIN
-	| typeof M2_DEVNET_CHAIN
-	| typeof M2_TESTNET_CHAIN
-	| typeof M2_LOCALNET_CHAIN
-	| typeof M2_MAINNET_CHAIN;
+  | typeof M1_DEVNET_CHAIN
+  | typeof M1_TESTNET_CHAIN
+  | typeof M1_LOCALNET_CHAIN
+  | typeof M1_MAINNET_CHAIN
+  | typeof M2_DEVNET_CHAIN
+  | typeof M2_TESTNET_CHAIN
+  | typeof M2_LOCALNET_CHAIN
+  | typeof M2_MAINNET_CHAIN
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from '@wallet-standard/core';
+export * from '@wallet-standard/core'
 
-export type { Wallet } from './wallet';
-export * from './features';
-export * from './detect';
-export * from './chains';
+export type { Wallet } from './wallet'
+export * from './features'
+export * from './detect'
+export * from './chains'
